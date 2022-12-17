432a48b0eafbf54cf55c6b73de44548cf7057ecc
Angela7Bel <119677186+Angela7Bel@users.noreply.github.com>
2022-12-18 01:06:48 +0700

    Add files via upload

---
+++ Задача 2/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее
+Console.Clear();
+Console.Write("а =");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("b =");
+int b = Convert.ToInt32(Console.ReadLine());
+ if (a > b) 
+    Console.WriteLine("a = max b = min");
+ else
+ if (b > a)
+    Console.WriteLine("b = max a = min");
+
+
+
---
+++ Задача 4/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+Console.Clear();
+Console.Write("а =");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("b =");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("c =");
+int c = Convert.ToInt32(Console.ReadLine());
+int max = a;
+if (a > max ) max = a;
+if (b > max ) max = b;
+if (c > max ) max = c;
+Console.Write("max = ");
+Console.WriteLine(max);
+
