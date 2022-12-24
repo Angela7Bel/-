2c145ae03c718ca43aca116e3045a7f951bfa7d3
Angela7Bel <119677186+Angela7Bel@users.noreply.github.com>
2022-12-24 13:50:35 +0700

    Add files via upload

---
+++ Задача 13/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.Write("Введите число ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n<100) 
+    Console.WriteLine("Третьей цифры числа нет");
+else {
+    while (n>=1000) {
+        n=n/10;
+    }
+    n = n%10;
+    Console.WriteLine($"Третья цифра числа {n}");
+}
+
