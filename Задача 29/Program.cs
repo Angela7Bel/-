be9eb024dbedaf588afe02bc96fb934665e07055
Angela7Bel <119677186+Angela7Bel@users.noreply.github.com>
2022-12-31 13:51:40 +0700

    Add files via upload

---
+++ Задача 29/Program.cs
@@ -0,0 +1,21 @@
+﻿void PrintArray(int[] arr)
+{   
+    Console.Write("[");
+    for (int i = 0; i < arr.Length - 1; i++)
+        Console.Write($"{arr[i]}, ");
+    Console.WriteLine($"{arr[arr.Length - 1]}]");
+}
+Console.Clear();
+
+Console.Write("Введите размер массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+for (int i = 0; i < n; i++)
+{
+    Console.Write($"Введите {i+1}-й элемент массива: ");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+Console.WriteLine();
+Console.Write("Вы ввели массив: ");
+PrintArray(array);
