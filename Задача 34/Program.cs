ab86a8d3d949b611c2aac24576183aec227136a2
Angela7Bel <119677186+Angela7Bel@users.noreply.github.com>
2023-01-01 17:33:01 +0700

    Add files via upload

---
+++ Задача 34/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n > 99 || n < 1000);
+{
+n = / 10;
+n = % 10;
+Console.WriteLine();
+}
+else
+    Console.WriteLine("не трёхзначное число");
