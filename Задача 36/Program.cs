7acf73ba832dac57892038794d93269e80148c54
Angela7Bel <119677186+Angela7Bel@users.noreply.github.com>
2023-01-01 17:33:17 +0700

    Add files via upload

---
+++ Задача 36/Program.cs
@@ -0,0 +1,25 @@
+﻿// Вычисляем сумму элементов на нечетных позициях
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length - 1; i++)
+        Console.Write($"{arr[i]}, ");
+    Console.WriteLine($"{arr[arr.Length - 1]}]");
+}
+
+Console.Clear();
+Console.Write("Введите размер массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+for (int i = 0; i < n; i++)
+    array[i] = new Random().Next(0, 200);
+
+Console.Write("Исходный массив: ");
+PrintArray(array);
+
+int sum = 0;
+for (int i = 1; i < n; i+=2)
+    sum += array[i];
+
+Console.WriteLine($"Сумма элементов на нечетных позициях = {sum}.");
