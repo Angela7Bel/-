27be94260b636a27433c013bd6fc7a52d4a364c9
Angela7Bel <119677186+Angela7Bel@users.noreply.github.com>
2023-01-13 02:14:44 +0700

    Add files via upload

---
+++ Задача 64/Program.cs
@@ -0,0 +1,14 @@
+﻿string GetNumbers(int start, int end)
+{
+    if (start == end) return end.ToString();
+    return (start < end) ? GetNumbers(start + 1, end) + $", {start}": 
+                           GetNumbers(start, end + 1) + $", {end}";
+}
+
+Console.Clear();
+Console.Write("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Числа: {GetNumbers(m, n)}");
---
+++ Задача 66/Program.cs
@@ -0,0 +1,14 @@
+﻿int GetSum(int start, int end)
+{
+    if (start == end) return end;
+    return (start < end) ? GetSum(start + 1, end) + start: 
+                           GetSum(start, end + 1) + end;
+}
+
+Console.Clear();
+Console.Write("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Сумма чисел от {m} до {n} = {GetSum(m, n)}");
